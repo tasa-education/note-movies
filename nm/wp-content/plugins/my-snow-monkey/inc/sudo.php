80d16fee7c2b4eaf4cfa8f68dd7fcf08ea9bbf60
Mariko Sudo <sudo@fairground.jp>
2020-05-17 17:30:19 +0900

    フッターにクレジットを追加

--- nm/wp-content/plugins/my-snow-monkey/inc/sudo.php
+++ nm/wp-content/plugins/my-snow-monkey/inc/sudo.php
@@ -1,18 +1,19 @@
 <?php
 /**
- * 検索機能カスタマイズ
+ * すどう対応分
  *
  * @package do_actionJapan2020
  * @since 1.0.0
  */
 
 
-if ( !class_exists( 'noteMoviesSearch' ) ) :
-class noteMoviesSearch {
+if ( !class_exists( 'noteMoviesSudou' ) ) :
+class noteMoviesSudou {
 
 
 	public function __construct() {
 		add_action( 'pre_get_posts', array( $this, 'get_posts_by_post_type' ) );
+		add_action( 'wp_footer', array( $this, 'add_credits' ), 9999 );
 	}
 
 
@@ -25,6 +26,33 @@ class noteMoviesSearch {
 		}
 		return $query;
 	}
+
+
+	/**
+	 * フッターにクレジットを追加
+	 */
+	public function add_credits() {
+echo <<< EOM
+
+<!--
+--------
+------------
+このサイトは、WordPress を使って地域社会を盛り上げるための
+チャリティーハッカソン 「do_action Japan 2020」で集まった有志により作成されました。
+
+【メンバー】
+ディレクター - 大串 肇
+デザイナー兼ライター - 伊東 けいこ
+開発者 - 静海 義明
+SNS担当 - 鈴木 毅
+テスター - 幸田 良佑
+開発者 - すどう まりこ
+------------
+--------
+-->
+
+EOM;
+	}
 }
-new noteMoviesSearch();
+new noteMoviesSudou();
 endif;
