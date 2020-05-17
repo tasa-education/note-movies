47bec784f8c0d19e029e1cc74705eef594baf49a
ShizumiYoshiaki <ni.muzihs@shizum.in>
2020-05-17 10:57:49 +0900

    :+1: プラグイン調整

---
+++ nm/wp-content/plugins/simple-custom-css/includes/public.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Front-facing functionality.
+ *
+ * @package SCCSS
+ * @since   2.0.0
+ * @author  John Regan <john@johnregan3.com>
+ */
+
+// Prevent direct file access.
+if ( ! defined( 'SCCSS_FILE' ) ) {
+	die();
+}
+
+/**
+ * Enqueue link to add CSS through PHP.
+ *
+ * This is a typical WP Enqueue statement, except that the URL of the stylesheet is simply a query var.
+ * This query var is passed to the URL, and when it is detected by scss_maybe_print_css(),
+ * it writes its PHP/CSS to the browser.
+ *
+ * @since  1.0.0
+ *
+ * @action wp_enqueue_scripts, 99
+ */
+function sccss_register_style() {
+	$url = home_url();
+
+	if ( is_ssl() ) {
+		$url = home_url( '/', 'https' );
+	}
+
+	wp_register_style( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters
+		'sccss_style',
+		add_query_arg(
+			array(
+				'sccss' => 1,
+			),
+			$url
+		)
+	);
+
+	wp_enqueue_style( 'sccss_style' );
+}
+
+add_action( 'wp_enqueue_scripts', 'sccss_register_style', 99 );
+
+/**
+ * If the query var is set, print the Simple Custom CSS rules.
+ *
+ * @since  1.0.0
+ *
+ * @action plugins_loaded
+ */
+function sccss_maybe_print_css() {
+
+	// Only print CSS if this is a stylesheet request.
+	if ( ! isset( $_GET['sccss'] ) || intval( $_GET['sccss'] ) !== 1 ) {  // phpcs:ignore WordPress.Security.NonceVerification
+		return;
+	}
+
+	ob_start();
+	header( 'Content-type: text/css' );
+
+	sccss_the_css();
+
+	die();
+}
+add_action( 'plugins_loaded', 'sccss_maybe_print_css' );
+
+/**
+ * Echo the CSS.
+ *
+ * @since 4.0.0
+ */
+function sccss_the_css() {
+	$options     = get_option( 'sccss_settings' );
+	$raw_content = isset( $options['sccss-content'] ) ? $options['sccss-content'] : '';
+	$content     = wp_kses( $raw_content, array( '\'', '\"' ) );
+	$content     = str_replace( '&gt;', '>', $content );
+	echo $content; // phpcs:ignore WordPress.Security.EscapeOutput
+}
