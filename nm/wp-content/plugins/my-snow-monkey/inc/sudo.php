196fd6abf310fd4468235d68b4109733264a7f84
megane9988 <info@m-g-n.me>
2020-05-17 23:30:14 +0900

    Revert "フッターにクレジットを追加"

--- nm/wp-content/plugins/my-snow-monkey/inc/sudo.php
+++ nm/wp-content/plugins/my-snow-monkey/inc/sudo.php
@@ -1,19 +1,18 @@
 <?php
 /**
- * すどう対応分
+ * 検索機能カスタマイズ
  *
  * @package do_actionJapan2020
  * @since 1.0.0
  */
 
 
-if ( !class_exists( 'noteMoviesSudou' ) ) :
-class noteMoviesSudou {
+if ( !class_exists( 'noteMoviesSearch' ) ) :
+class noteMoviesSearch {
 
 
 	public function __construct() {
 		add_action( 'pre_get_posts', array( $this, 'get_posts_by_post_type' ) );
-		add_action( 'wp_footer', array( $this, 'add_credits' ), 9999 );
 	}
 
 
@@ -26,33 +25,6 @@ class noteMoviesSudou {
 		}
 		return $query;
 	}
-
-
-	/**
-	 * フッターにクレジットを追加
-	 */
-	public function add_credits() {
-echo <<< EOM
-
-<!--
---------
-------------
-このサイトは、WordPress を使って地域社会を盛り上げるための
-チャリティーハッカソン 「do_action Japan 2020」で集まった有志により作成されました。
-
-【メンバー】
-ディレクター - 大串 肇
-デザイナー兼ライター - 伊東 けいこ
-開発者 - 静海 義明
-SNS担当 - 鈴木 毅
-テスター - 幸田 良佑
-開発者 - すどう まりこ
-------------
---------
--->
-
-EOM;
-	}
 }
-new noteMoviesSudou();
+new noteMoviesSearch();
 endif;
